c95247eac60c8559c636c7c77132119fcc1c348c
Lavanyaa K.R <96877605+Lavanyabts@users.noreply.github.com>
2022-04-15 12:47:28 +0530

    post

--- triangle.js
+++ triangle.js
@@ -1,24 +1,24 @@
-const inputs = document.querySelectorAll('.angle-input');
-const formTrainglebtn = document.querySelector('#form-traingle-btn');
-const outputE1 = document.querySelector('#output');
+const input = document.querySelectorAll('.angle-input');
+const traingleBtn = document.querySelector('#traingle-btn');
+const output = document.querySelector('.output');
 
 function sumOfAngles (angle1,angle2,angle3) {
     const sum = angle1 + angle2 + angle3; 
     return sum;
 }
 
-function formTriangle() {
-    const sum = sumOfAngles (Number(inputs[0].value),
-     Number(inputs[1].value), 
-     Number(inputs[2].value)
+function triangle() {
+    const sum = sumOfAngles (Number(input[0].value),
+     Number(input[1].value), 
+     Number(input[2].value)
      );
     if (sum === 180) {
-        outputE1.innerText = "angle forms the triangle dude"
+        output.innerText = "angle forms the triangle dude"
     }
     else {
-        outputE1.innerText = "it dosen't form a Triangle dude"
+        output.innerText = "it dosen't form a Triangle dude"
     }
 }
 
-formTrainglebtn.addEventListener("click",formTriangle);
+traingleBtn.addEventListener("click",triangle);
 
