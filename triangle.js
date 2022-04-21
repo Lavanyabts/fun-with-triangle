586d72c588d453d7c1c405fd9064d1dfbbd9e2dc
Lavanyaa K.R <96877605+Lavanyabts@users.noreply.github.com>
2022-04-21 19:21:32 +0530

    push

--- triangle.js
+++ triangle.js
@@ -1,12 +1,10 @@
 const input = document.querySelectorAll('.angle-input');
 const triangleBtn = document.querySelector('#triangle-btn');
 const output = document.querySelector('#output');
-
 function sumOfAngles (angle1,angle2,angle3) {
     const sum = angle1 + angle2 + angle3; 
     return sum;
 }
-
 function triangle() {
     const sum = sumOfAngles (Number(input[0].value),
      Number(input[1].value), 
@@ -19,6 +17,4 @@ function triangle() {
         output.innerText = "it dosen't form a Triangle dude";
     }
 }
-
 triangleBtn.addEventListener("click",triangle);
-
